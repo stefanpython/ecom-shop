d7d2828cf002775287b5afb7f22b7d714f6b48a0
stefanpython <stefanhtml@yahoo.com>
2025-04-24 14:06:54 +0300

    Create protected route component

---
+++ client/src/components/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const ProtectedRoute = () => {
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        Loading...
+      </div>
+    );
+  }
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
