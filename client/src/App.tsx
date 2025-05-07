f07812ffc0d93ffd94f79db69e1df5bf0b2ba579
stefanpython <stefanhtml@yahoo.com>
2025-05-07 10:37:21 +0300

    Update app.tsx to include admin routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,19 @@ import ProfilePage from "./pages/ProfilePage";
 import OrdersPage from "./pages/OrdersPage";
 import OrderDetailPage from "./pages/OrderDetailPage";
 import ProtectedRoute from "./components/ProtectedRoute";
+import AdminRoute from "./components/AdminRoute";
+import AdminLayout from "./components/AdminLayout";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminProducts from "./pages/admin/AdminProducts";
+import AdminProductEdit from "./pages/admin/AdminProductEdit";
+import AdminCategories from "./pages/admin/AdminCategories";
+import AdminCategoryEdit from "./pages/admin/AdminCategoryEdit";
+import AdminOrders from "./pages/admin/AdminOrders";
+import AdminOrderDetail from "./pages/admin/AdminOrderDetail";
+import AdminUsers from "./pages/admin/AdminUsers";
+import AdminUserEdit from "./pages/admin/AdminUserEdit";
+import AdminReviews from "./pages/admin/AdminReviews";
+import AdminPayments from "./pages/admin/AdminPayments";
 import { Toaster } from "react-hot-toast";
 
 function App() {
@@ -32,6 +45,25 @@ function App() {
             <Route path="profile" element={<ProfilePage />} />
             <Route path="orders" element={<OrdersPage />} />
             <Route path="orders/:id" element={<OrderDetailPage />} />
+          </Route>
+        </Route>
+
+        {/* Admin Routes */}
+        <Route element={<AdminRoute />}>
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<AdminDashboard />} />
+            <Route path="products" element={<AdminProducts />} />
+            <Route path="products/new" element={<AdminProductEdit />} />
+            <Route path="products/:id" element={<AdminProductEdit />} />
+            <Route path="categories" element={<AdminCategories />} />
+            <Route path="categories/new" element={<AdminCategoryEdit />} />
+            <Route path="categories/:id" element={<AdminCategoryEdit />} />
+            <Route path="orders" element={<AdminOrders />} />
+            <Route path="orders/:id" element={<AdminOrderDetail />} />
+            <Route path="users" element={<AdminUsers />} />
+            <Route path="users/:id" element={<AdminUserEdit />} />
+            <Route path="reviews" element={<AdminReviews />} />
+            <Route path="payments" element={<AdminPayments />} />
           </Route>
         </Route>
       </Routes>
