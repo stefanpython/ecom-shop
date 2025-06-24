ebac1429a9673ce4f7d44042b82101463e49c7a8
stefanpython <stefanhtml@yahoo.com>
2025-06-24 15:06:46 +0300

    Change value

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -9,7 +9,7 @@ const User = require("../models/userModel");
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 8;
+  const pageSize = 9;
   const page = Number(req.query.pageNumber) || 1;
 
   const keyword = req.query.keyword
