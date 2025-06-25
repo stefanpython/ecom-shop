a0da25ca3487173b990b005941ab363041179e38
stefanpython <stefanhtml@yahoo.com>
2025-06-25 14:42:23 +0300

    Configure CORS

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://stefanpython.github.io",
     credentials: true,
   })
 );
