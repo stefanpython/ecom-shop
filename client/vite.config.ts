192bfdf38f44f1be998ec5813d193b25a3cef02e
stefanpython <stefanhtml@yahoo.com>
2025-06-25 14:31:45 +0300

    Configure gh-pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ecom-shop/",
+  base: "https://stefanpython.github.io/ecom-shop/",
   plugins: [react(), tailwindcss()],
 });
