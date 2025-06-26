430d52eb77ab27c18f7907014bb1d3f6d233589c
stefanpython <stefanhtml@yahoo.com>
2025-06-26 13:19:01 +0300

    Add CORS for localhost

--- server/app.js
+++ server/app.js
@@ -39,7 +39,10 @@ app.use(express.static(path.join(__dirname, "public")));
 // CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "https://stefanpython.github.io",
+    origin:
+      process.env.CLIENT_URL ||
+      "https://stefanpython.github.io" ||
+      "http://localhost:5173",
     credentials: true,
   })
 );
